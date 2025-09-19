4349b41756f0c1b259293f9077c5c94e456b8aac
MarceloRDPJ <marceloprego1223@gmail.com>
2025-09-19 11:24:21 -0300

    Create service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,21 @@
+const CACHE_NAME = 'tecnoit-portal-v1';
+const urlsToCache = [
+  '/',
+  'index.html',
+  '[https://cdn.tailwindcss.com](https://cdn.tailwindcss.com)',
+  '[https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600;700&display=swap](https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600;700&display=swap)',
+  '[https://unpkg.com/react@18/umd/react.development.js](https://unpkg.com/react@18/umd/react.development.js)',
+  '[https://unpkg.com/react-dom@18/umd/react-dom.development.js](https://unpkg.com/react-dom@18/umd/react-dom.development.js)',
+  '[https://unpkg.com/@babel/standalone/babel.min.js](https://unpkg.com/@babel/standalone/babel.min.js)',
+  '[https://cdn.skypack.dev/lucide-react](https://cdn.skypack.dev/lucide-react)'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache)));
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    fetch(event.request).catch(() => caches.match(event.request))
+  );
+});
