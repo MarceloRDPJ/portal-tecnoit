6f05ca2afc0259c9490691552811231792e990b9
MarceloRDPJ <marceloprego1223@gmail.com>
2025-09-19 11:39:40 -0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -2,16 +2,20 @@ const CACHE_NAME = 'tecnoit-portal-v1';
 const urlsToCache = [
   '/',
   'index.html',
-  '[https://cdn.tailwindcss.com](https://cdn.tailwindcss.com)',
-  '[https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600;700&display=swap](https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600;700&display=swap)',
-  '[https://unpkg.com/react@18/umd/react.development.js](https://unpkg.com/react@18/umd/react.development.js)',
-  '[https://unpkg.com/react-dom@18/umd/react-dom.development.js](https://unpkg.com/react-dom@18/umd/react-dom.development.js)',
-  '[https://unpkg.com/@babel/standalone/babel.min.js](https://unpkg.com/@babel/standalone/babel.min.js)',
-  '[https://cdn.skypack.dev/lucide-react](https://cdn.skypack.dev/lucide-react)'
+  'https://cdn.tailwindcss.com',
+  'https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600;700&display=swap',
+  'https://unpkg.com/react@18/umd/react.development.js',
+  'https://unpkg.com/react-dom@18/umd/react-dom.development.js',
+  'https://unpkg.com/@babel/standalone/babel.min.js',
+  'https://cdn.skypack.dev/lucide-react'
 ];
 
 self.addEventListener('install', event => {
-  event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache)));
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(urlsToCache);
+    })
+  );
 });
 
 self.addEventListener('fetch', event => {
